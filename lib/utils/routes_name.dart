6f8f287b8e0d721b77ce30e1de5c90b66129224b
Yousaf Manan <75005065+Yousafmanan@users.noreply.github.com>
2024-10-04 14:18:25 +0500

    added 2 new classes for organized routes and used in homescreen

---
+++ lib/utils/routes_name.dart
@@ -0,0 +1,10 @@
+
+class RouteName {
+
+  static const String homeScreen = 'home_screen';
+  static const String historyScreen = 'history_screen';
+  static const String scheduleScreen = 'schedule_screen';
+  static const String teamsScreen = 'teams_screen';
+  static const String venuesScreen = 'venues_screen';
+
+}
